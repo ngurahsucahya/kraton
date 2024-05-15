edde1249eda236a829f538e4e9a510e49e82101a
Yuukasalsabila <yuukasalsabila@apps.ipb.ac.id>
2024-05-15 23:29:45 +0700

    logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -73,7 +73,7 @@
             <div class="row g-0 mx-lg-0">
                 <div class="col-lg-6 ps-lg-0 wow fadeIn" data-wow-delay="0.1s" style="min-height: 100px;">
                     <div class="position-relative h-100">
-                        <img class="position-absolute img-fluid w-100 h-100" src="{{ asset('template/img/20220805_123216.jpg')}}" style="object-fit: cover;" alt="">
+                        <img class="position-absolute img-fluid w-100 h-100" src="{{ asset('template/img/kratonlogo.jpeg')}}" style="object-fit: cover;" alt="">
                     </div>
                 </div>
                 <div class="col-lg-6 quote-text py-5 wow fadeIn" data-wow-delay="0.5s">
